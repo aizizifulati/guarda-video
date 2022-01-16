7a7142dd42167ff25a4fc07bbf2a061feafb579c
craazyjava <aziz0804@libero.it>
2022-01-16 01:50:58 -0500

    first commit

---
+++ backend/src/main/java/com/programming/aizizi/guardavideo/GuardaVideoApplication.java
@@ -0,0 +1,13 @@
+package com.programming.aizizi.guardavideo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuardaVideoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuardaVideoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/programming/aizizi/guardavideo/GuardaVideoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programming.aizizi.guardavideo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuardaVideoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
